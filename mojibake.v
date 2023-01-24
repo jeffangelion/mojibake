cbd318b903e0afc583353893050a7267aa0edbf9
Ivan Vatlin <jenrus+codeberg@riseup.net>
2023-01-24 18:47:51 +0300

    Mojibake v1.2.0 - now supports UTF-8 to JIS X 0213 conversion

--- mojibake.v
+++ mojibake.v
@@ -1,5 +1,30 @@
 module mojibake
 
+pub fn utf8_to_jis_x_0213(input string) []u8 {
+	input_runes := input.runes()
+	mut output := []u8{}
+	mut i := 0
+	for i < input_runes.len {
+		match input_runes[i] {
+			0x20 ... 0x5B, 0x5D ... 0x7D {
+				output << input_runes[i].str()[0]
+				i++
+			}
+			else {
+				// is character part of unicode sequence?
+				if utf8_possible_sequences_jis_x_0213[input[i]].index(input[i + 1]) > -1 && i + 1 < input.len {
+					output << utf8_sequence_for_jis_x_0213[input_runes[i..i+2].string()]
+					i += 2
+				} else {
+					output << utf8_to_jis_x_0213_data[input_runes[i]]
+					i++
+				}
+			}
+		}
+	}
+	return output
+}
+
 pub fn jis_x_0213_to_utf8(input []u8) string {
 	mut output := ''
 	mut i := 0
--- v.mod
+++ v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'mojibake'
-	description: 'Mojibake is V library JIS X 0213:2004 -> UTF-8 text encoding conversion'
-	version: '1.1.0'
-	license: 'LGPL'
+	description: 'Mojibake is V library JIS X 0213:2004 ↔ UTF-8 text encoding conversion'
+	version: '1.2.0'
+	license: 'LGPL-3.0-or-later'
 	dependencies: []
 }
